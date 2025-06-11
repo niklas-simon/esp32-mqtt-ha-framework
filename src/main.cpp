32e7c111aa63cbcde4aff3fd478d0c16f55c19c1
Niklas <niklaspein12@gmail.com>
2025-06-11 13:33:33 +0200

    add example main

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,21 @@
 #include <HomeAssistant.h>
 
 HomeAssistant ha = HomeAssistant();
+Device example = Device("561070cc-d216-4a10-9417-b14ee20db271", "Example Device", "ESP32-C3", "livingroom", "example");
+
+void update_state() {
+
+}
 
 void setup()
 {
     Serial.begin(9600);
-    while (!Serial);
 
+    ha.configure(&example, update_state);
     ha.begin();
 }
 
 void loop()
 {
-    delay(10);
+    ha.loop();
 }
